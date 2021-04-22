797e3f5a2c6fd2c797d9f22feca59ee1c306de2c
Fahad Khokhar <fahadabbasi2002@gmail.com>
2021-04-22 14:48:55 +0200

    -

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,13 +1,13 @@
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
-VALUES (1, 'Henk', 'van Balen', '0D', 'Albert Cuypstraat 1' , '1234AB', 2001-06-19, 'Amsterdam');
+VALUES (1, 'Henk', 'van Balen', '0D', 'Albert Cuypstraat 1' , '1234AB', '2001-06-19', 'Amsterdam');
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
-VALUES (5, 'Piet', 'Pieters', '0D', 'Albert Cuypstraat 3' , '1243BA', 2001-06-19, 'Amsterdam');
+VALUES (5, 'Piet', 'Pieters', '0D', 'Albert Cuypstraat 3' , '1243BA', '2001-06-19', 'Amsterdam');
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats) 
-VALUES (6, 'Johanna', 'Eduardo', '0A', 'Pieter Aertszstraat 44' , '1073CD', 2001-09-02, 'Hoorn');
+VALUES (6, 'Johanna', 'Eduardo', '0A', 'Pieter Aertszstraat 44' , '1073CD', '2001-09-02', 'Hoorn');
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats) 
-VALUES (7, 'Angela', 'Mohammed', '0D', 'Piet Modriaanplien 44' , '1299CD', 2002-05-17, 'Amstelveen');
+VALUES (7, 'Angela', 'Mohammed', '0D', 'Piet Modriaanplien 44' , '1299CD', '2002-05-17', 'Amstelveen');
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats) 
-VALUES (8, 'Fahad', 'Khokhar', '0D', 'Johannes Calvijnlaan 66' , '1185BM', 2002-07-16, 'Amstelveen');
+VALUES (8, 'Fahad', 'Khokhar', '0D', 'Johannes Calvijnlaan 66' , '1185BM', '2002-07-16', 'Amstelveen');
 INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats) 
-VALUES (9, 'Jan', 'Willem', '0A', 'Piet Modriaanplein 24' , '1299CD', 2001-06-19, 'Amstelveen');
+VALUES (9, 'Jan', 'Willem', '0A', 'Piet Modriaanplein 24' , '1299CD', '2001-06-19', 'Amstelveen');
 
