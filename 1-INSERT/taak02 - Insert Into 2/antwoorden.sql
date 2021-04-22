9edbe6c9bfd9efafc471e5d0c486b2b0df0ec233
Fahad Khokhar <fahadabbasi2002@gmail.com>
2021-04-22 15:02:23 +0200

    -

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO studenten (voornaam, achternaam , geboortedatum, woonplaats) 
+VALUES ('Samy', 'Farahat', '2003-02-12', 'Amstelveen');
+INSERT INTO studenten (voornaam, achternaam , geboortedatum, woonplaats) 
+VALUES ('Arman', 'Salehi', '2000-01-01', 'Amstelveen');
+INSERT INTO studenten (voornaam, achternaam , geboortedatum, woonplaats) 
+VALUES ('Julian', 'de Vries', '2001-02-02', 'Amstelveen');
